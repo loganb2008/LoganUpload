0e5fe78fc0d44160ff141ed540579e70ee850d0f
Logan Begley <91550229+loganb2008@users.noreply.github.com>
2021-09-29 11:08:13 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $config = array(
     "imgpath" => "/loganupload/uploads/", // Put the location of your image directory here (NOT FULL PATH)
-    "pagetitle" => "Your HxPload Panel", // Put the title of the page here
-    "pagename" => "HxPload", // Put want you want the title text on the page to say
+    "pagetitle" => "Your LoganUpload Panel", // Put the title of the page here
+    "pagename" => "LoganUpload", // Put want you want the title text on the page to say
     "fterror" => "That file type isn't allowed. Must be an image. Change this in the config.php file.", // Error message for invalid file type
     "tlerror" => "That file is too large. Change this in the config file.", // Error message for too large
     "fakeimageerror" => "That doesn't seem to be an image. Please select an image. Change this in the config file", // Fake image error
