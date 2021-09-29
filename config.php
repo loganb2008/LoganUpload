604c551c6273c2e798289ca4040f7462ea3ad984
Logan Begley <91550229+loganb2008@users.noreply.github.com>
2021-09-29 09:50:57 -0400

    Create default config

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$config = array(
+    "imgpath" => "/loganupload/uploads/", // Put the location of your image directory here (NOT FULL PATH)
+    "pagetitle" => "Your LoganUpload Panel", // Put the title of the page here
+    "pagename" => "LoganUpload", // Put want you want the title text on the page to say
+    "fterror" => "That file type isn't allowed. Must be an image. Change this in the config.php file.", // Error message for invalid file type.
+    "tlerror" => "That file is too large. Change this in the config file.", // Error message for too large
+    "fakeimageerror" => "That doesn't seem to be an image. Please select an image. Change this in the config file", // Fake image error
+    "maxsize" => 10000000 // Max file size in bytes. Default 10MB (10000000 Bytes)
+);
